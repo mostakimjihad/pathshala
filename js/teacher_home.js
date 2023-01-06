e0159048aff382987e2c66827873c92ac2ddf883
Mostakim JIhad <74228709+mostakimjihad@users.noreply.github.com>
2023-01-06 15:13:06 +0600

    sds

---
+++ js/teacher_home.js
@@ -0,0 +1,7 @@
+function show(){
+   document.getElementById('add-class-popup').style.display = 'flex';
+}
+
+function hide(){
+    document.getElementById('add-class-popup').style.display = 'none';
+}
